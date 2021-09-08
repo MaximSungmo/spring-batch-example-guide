f1d16847bb774af0da827eabdd82b07bd0e5da93
MaximSungmo <ksm5318@naver.com>
2021-09-08 22:43:13 +0900

    feat: ch4 MethodInvokingTaskletConfiguration with parameters

--- src/main/java/com/example/batch/demo/ch4/CustomService.java
+++ src/main/java/com/example/batch/demo/ch4/CustomService.java
@@ -2,7 +2,8 @@ package com.example.batch.demo.ch4;
 
 public class CustomService {
 
-    public void serviceMethod() {
+    public void serviceMethod(String message) {
         System.out.println("Service method was called");
+        System.out.println(message);
     }
 }
--- src/main/java/com/example/batch/demo/ch4/MethodInvokingTaskletConfiguration.java
+++ src/main/java/com/example/batch/demo/ch4/MethodInvokingTaskletConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,15 +25,19 @@ public class MethodInvokingTaskletConfiguration {
 
     @Bean
     public Step methodInvokingStep() {
-        return this.stepBuilderFactory.get("methodInvokingStep").tasklet(methodInvokingTasklet()).build();
+        return this.stepBuilderFactory.get("methodInvokingStep").tasklet(methodInvokingTasklet(null)).build();
     }
 
+    @StepScope
     @Bean
-    public MethodInvokingTaskletAdapter methodInvokingTasklet() {
+    public MethodInvokingTaskletAdapter methodInvokingTasklet(
+            @Value("#{jobParameters['message']}") String message
+    ) {
         MethodInvokingTaskletAdapter methodInvokingTaskletAdapter = new MethodInvokingTaskletAdapter();
 
         methodInvokingTaskletAdapter.setTargetObject(service());
         methodInvokingTaskletAdapter.setTargetMethod("serviceMethod");
+        methodInvokingTaskletAdapter.setArguments(new String[]{message});
 
         return methodInvokingTaskletAdapter;
     }
