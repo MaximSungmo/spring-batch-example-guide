f706f08af6600e0843d093ca06d9d0ae08cf97e4
MaximSungmo <ksm5318@naver.com>
2021-10-28 13:10:58 +0900

    refactor: disable quarts, ch7

--- src/main/java/com/example/batch/demo/ch6/QuartzJobConfiguration.java
+++ src/main/java/com/example/batch/demo/ch6/QuartzJobConfiguration.java
@@ -1,36 +1,36 @@
-package com.example.batch.demo.ch6;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class QuartzJobConfiguration {
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job quartzJob() {
-        return jobBuilderFactory.get("quartzJob")
-                .incrementer(new RunIdIncrementer())
-                .start(quartzStep())
-                .build();
-    }
-
-    @Bean
-    public Step quartzStep() {
-        return stepBuilderFactory.get("quartzStep")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("[CH6] quartz step 1 ran today!");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-}
+//package com.example.batch.demo.ch6;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.launch.support.RunIdIncrementer;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class QuartzJobConfiguration {
+//    @Autowired
+//    private JobBuilderFactory jobBuilderFactory;
+//    @Autowired
+//    private StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job quartzJob() {
+//        return jobBuilderFactory.get("quartzJob")
+//                .incrementer(new RunIdIncrementer())
+//                .start(quartzStep())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step quartzStep() {
+//        return stepBuilderFactory.get("quartzStep")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                    System.out.println("[CH6] quartz step 1 ran today!");
+//                    return RepeatStatus.FINISHED;
+//                }).build();
+//    }
+//}
